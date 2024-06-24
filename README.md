b0e2cc64cf03e2e0e036bbc1790d9af3b444350d
tarwaxur <karahanbugrahan909@gmail.com>
2024-06-24 22:41:10 +0300

    Starter-2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Starter 2
  Mixed code experiments.
+
+Hi, I created this project to test mixed codes.
---
+++ app.js
@@ -0,0 +1,24 @@
+// ! Turkish: Denemek.
+// ! English: To try.
+
+let value
+
+alert("Hello, welcome.");
+
+alert("I will ask you to enter a number. Rest assured, I will know the number you enter.");
+
+alert("1, 2 or 3. Choose one.");
+
+prompt("1, 2, 3");
+
+if(value = 1){
+    alert("The value you entered 1");
+}
+
+if(value = 2){
+    alert("The value you entered 2");
+}
+
+if(value = 3){
+    alert("The value you entered");
+}
